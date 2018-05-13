fe098ece478e763c0651e72bfcb7e61481b5cacd
Boris Josz <Boris.Josz@gmail.com>
2018-05-13 20:45:43 +0200

    separate components structure base, in before form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Form.css';
+import FormSlide from './FormSlide.js';
 
 class Form extends React.Component {
   render() {
@@ -12,10 +13,13 @@ export default Form;
 // should I use const or let?
 const form =
 <div className="container">
+  <div className="formBody">
+    <FormSlide />
+  </div>
   <div className="form-background">
-    <div className="formtext title"> Launch a Check </div>
-    <div className="formtext next"> Next </div>
-    <div className="formtext previous"> Previous </div>
+    <div className="formText title"> Launch a Check </div>
+    <div className="formText next"> Next </div>
+    <div className="formText previous"> Previous </div>
   </div>
   <div className="image-container">
     <img src={'http://res.cloudinary.com/borisj/image/upload/v1526232024/Medicheck/MedicheckFormBackground.png'} className="background-image"/>
