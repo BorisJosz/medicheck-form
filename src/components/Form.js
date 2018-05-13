1d5757e91b0676aed59bcfaaefc7a41eead5bd3b
Boris Josz <Boris.Josz@gmail.com>
2018-05-13 19:29:17 +0200

    Form.js basic structure complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,23 @@
 import React from "react";
+import './Form.css';
 
 class Form extends React.Component {
   render() {
-    return <div> This is where I'll build my form </div>
+    return form
   }
 }
 
 export default Form;
+
+// should I use const or let?
+const form =
+<div className="container">
+  <div className="form-background">
+    <div className="formtext title"> Launch a Check </div>
+    <div className="formtext next"> Next </div>
+    <div className="formtext previous"> Previous </div>
+  </div>
+  <div className="image-container">
+    <img src={'http://res.cloudinary.com/borisj/image/upload/v1526232024/Medicheck/MedicheckFormBackground.png'} className="background-image"/>
+  </div>
+</div>
