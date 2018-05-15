5b2314fe2337b58f518ed9a53f9a9db1dc7e8051
Boris Josz <Boris.Josz@gmail.com>
2018-05-15 17:18:29 +0200

    flexbox implementation WIP

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -27,8 +27,6 @@ class Form extends React.Component {
           <div className="form-background">
             <div className="formText title"> Launch a Check </div>
               <form id="msform">
-                {/* progressbar */}
-                <Stepper currentStep={this.state.currentStep} numberOfStep={this.state.numberOfStep}/>
 
                 {/* fieldsets */}
                 {this.state.currentStep === 1 &&
@@ -36,8 +34,6 @@ class Form extends React.Component {
                     <h2 className="fs-title"> Company Information </h2>
                     <input type="text" name="contactPerson" placeholder="Contact person's name" />
                     <input type="text" name="contactEmail" placeholder="Email address" />
-
-
                   </fieldset>
                 }
 
@@ -47,18 +43,12 @@ class Form extends React.Component {
                     <input type="text" name="firstName" placeholder="First Name" />
                     <input type="text" name="lastName" placeholder="Last Name" />
                     <input type="text" name="employeeEmail" placeholder="Email address" />
-
-
-
                   </fieldset>
                 }
 
                 {this.state.currentStep === 3 &&
                   <fieldset>
                     <h2 className="fs-title"> Check Details </h2>
-
-
-
                   </fieldset>
                 }
 
@@ -67,9 +57,6 @@ class Form extends React.Component {
                     <h2 className="fs-title"> Incapacity Period </h2>
                     <input type="text" name="startDate" placeholder="Start Date" />
                     <input type="text" name="endDate" placeholder="End Date" />
-
-
-
                   </fieldset>
                 }
 
@@ -78,15 +65,19 @@ class Form extends React.Component {
                     <h2 className="fs-title"> Comments </h2>
                     <input type="text" name="doctorComment" placeholder="This comment will only be seen by the concerned doctor" />
                     <input type="text" name="medicheckComment" placeholder="This comment will only be seen by Medicheck" />
-
                   </fieldset>
                 }
 
-                <button className="next action-button" onClick={this.nextStep}> Next </button>
-                {/* <button className="prev action-button" onClick={this.prevStep}> Previous </button>
-                 <input type="submit" name="submit" className="submit action-button" value="Submit" /> */}
 
               </form>
+
+              <button className="next action-button" onClick={this.nextStep}> Next </button>
+              {/* <button className="prev action-button" onClick={this.prevStep}> Previous </button>
+               <input type="submit" name="submit" className="submit action-button" value="Submit" /> */}
+
+              {/* progressbar */}
+              <Stepper currentStep={this.state.currentStep} numberOfStep={this.state.numberOfStep}/>
+
             </div>
           </div>
         <div className="image-container">
