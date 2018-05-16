711e2e1935e3fe317f31321c2c2c7866a60be6ba
Boris Josz <Boris.Josz@gmail.com>
2018-05-16 15:28:46 +0200

    added toggle component + lots of toggle related css

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Form.css';
 import Stepper from '../components/Stepper.js';
+import Toggle from '../components/Toggle.js'
 
 class Form extends React.Component {
   constructor(props){
@@ -52,26 +53,52 @@ class Form extends React.Component {
                     <input type="text" id="employeeLastName" placeholder="Last Name" />
                     <input type="text" id="employeeEmail" placeholder="Email address" />
                     <input type="text" id="employeePhoneNumber" placeholder="Phone number" />
+                    <div className="languageQuestion"> The employee speaks </div>
+                    <div className="languageQuestion fr"> FR </div>
+                    <div className="languageQuestion nl"> NL </div>
+                    <div className="toggleLanguage">
+                      <Toggle />
+                    </div>
                   </div>
                 </fieldset>
               }
               {currentStep === 3 &&
                 <fieldset class={this.state.animationSide}>
-                  <h2 className="fs-title"> Check Details </h2>
+                  <div className="grid-display">
+                    <h2 className="fs-title"> Check Details </h2>
+                    <div className="toggleTitles optimizedCheck"> Optimized Check </div>
+                    <div className="toggleDetailsText optimized"> Allows for the check to take place at the most opportune time of the incapacity period to maximize the chances of an early return to work </div>
+                    <div className="toggle-1">
+                      <Toggle />
+                    </div>
+                    <div className="toggleTitles immediateCheck"> Immediate Check </div>
+                    <div className="toggleDetailsText immediate"> Allows for the check to happen in the shortest delays possible </div>
+
+                    <div className="toggleTitles doctorsOffice"> At the doctor's cabinet </div>
+                    <div className="toggleDetailsText doctor"> We recommend this choice as it is more respectful of the employee’s intimacy and is thus in accordacne with the positive approach that your employer has chosen to partake in </div>
+                    <div className="toggle-2">
+                      <Toggle />
+                    </div>
+                    <div className="toggleTitles atHome"> At the employee's home </div>
+                    <div className="toggleDetailsText home"> Checks at home are recommended only in cases where the employee is required to stay home </div>
+
+                  </div>
                 </fieldset>
               }
               {currentStep === 4 &&
-              <fieldset class={this.state.animationSide}>
-                  <h2 className="fs-title"> Incapacity Period </h2>
-                  <input type="text" name="startDate" placeholder="Start Date" />
-                  <input type="text" name="endDate" placeholder="End Date" />
+                <fieldset class={this.state.animationSide}>
+                  <div className="grid-display">
+                    <h2 className="fs-title"> Incapacity Period </h2>
+
+                  </div>
                 </fieldset>
               }
               {currentStep === 5 &&
                 <fieldset class={this.state.animationSide}>
-                  <h2 className="fs-title"> Comments </h2>
-                  <input type="text" name="doctorComment" placeholder="This comment will only be seen by the concerned doctor" />
-                  <input type="text" name="medicheckComment" placeholder="This comment will only be seen by Medicheck" />
+                  <div className="grid-display">
+                    <h2 className="fs-title"> Comments </h2>
+
+                    </div>
                 </fieldset>
               }
             </form>
