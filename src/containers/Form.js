41a855d3ed1f0809421383872efd95765570174e
Boris Josz <Boris.Josz@gmail.com>
2018-05-23 19:38:20 +0200

    added css toggle switch

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './Form.css';
 import Stepper from '../components/Stepper.js';
-// import Toggle from '../components/Toggle.js';
 import '../components/Toggle.css';
 
 // added stuff
@@ -27,10 +26,31 @@ class Form extends React.Component {
       commentMedicheck: "",
       startDate: undefined,
       endDate: undefined,
-      employeeLanguage: true,
-      optimizedCheck: true,
-      atHome: true,
+      employeeLanguage: false,
+      optimizedCheck: false,
+      atHome: false,
     };
+  }
+
+  toggleCssLanguage = () => {
+    let elementFr = document.getElementById("fr");
+    elementFr.classList.toggle("toggleActive");
+    let elementNl = document.getElementById("nl");
+    elementNl.classList.toggle("toggleActive");
+  }
+
+  toggleCssOptimized = () => {
+    let elementOp = document.getElementById("optimized");
+    elementOp.classList.toggle("toggleActive");
+    let elementIm = document.getElementById("immediate");
+    elementIm.classList.toggle("toggleActive");
+  }
+
+  toggleCssHome = () => {
+    let elementDo = document.getElementById("doctorsOffice");
+    elementDo.classList.toggle("toggleActive");
+    let elementAh = document.getElementById("atHome");
+    elementAh.classList.toggle("toggleActive");
   }
 
   handleCheck = event => {
@@ -75,13 +95,13 @@ class Form extends React.Component {
         employeeLastName: this.state.employeeLastName,
         employeePhoneNumber: this.state.employeePhoneNumber,
         employeeAddress: this.state.employeeAddress,
-        commentDoctor: this.state.commentDoctor,
-        commentMedicheck: this.state.commentMedicheck,
-        startDate: this.state.startDate,
-        endDate: this.state.endDate,
         employeeLanguage: this.state.employeeLanguage,
         optimizedCheck: this.state.optimizedCheck,
         atHome: this.state.atHome,
+        startDate: this.state.startDate,
+        endDate: this.state.endDate,
+        commentDoctor: this.state.commentDoctor,
+        commentMedicheck: this.state.commentMedicheck,
       },
       config: { headers: {'Content-Type': 'application/json' }}
     })
@@ -123,13 +143,15 @@ class Form extends React.Component {
                     <input id="employeePhoneNumber" name="employeePhoneNumber" type="text" value={this.state.employeePhoneNumber} onChange={this.handleInputChange} placeholder="Phone number" />
                     <input id="employeeAddress" name="employeeAddress" type="text" value={this.state.employeeAddress} onChange={this.handleInputChange} placeholder="Street Address" />
                     <div className="languageQuestion"> The employee speaks </div>
-                    <div className="languageQuestion fr"> FR </div>
-                    <div className="languageQuestion nl"> NL </div>
+
+                    {/* Toggle options */}
+                    <div id="fr" className="languageQuestion fr toggleActive"> FR </div>
+                    <div id="nl" className="languageQuestion nl"> NL </div>
 
                     {/* TOGGLE */}
                     <div className="toggleLanguage">
                       <label className="switch">
-                        <input name="employeeLanguage" type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.employeeLanguage} value={this.state.employeeLanguage} />
+                        <input onClick={this.toggleCssLanguage} name="employeeLanguage" type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.employeeLanguage} value={this.state.employeeLanguage} />
                         <div className="slider"></div>
                       </label>
                     </div>
@@ -141,28 +163,32 @@ class Form extends React.Component {
                 <fieldset className={this.state.animationSide}>
                   <div className="grid-display">
                     <h2 className="fs-title"> Check Details </h2>
-                    <div className="toggleTitles optimizedCheck"> Optimized Check </div>
-                    <div className="toggleDetailsText optimized"> Allows for the check to take place at the most opportune time of the incapacity period to maximize the chances of an early return to work </div>
+
+                    {/* Toggle Options */}
+                      <div id="optimized" className="toggleTitles optimizedCheck toggleActive" title="Allows for the check to take place at the most opportune time of the incapacity period to maximize the chances of an early return to work"> Optimized Check </div>
+                      {/* <div className="toggleDetailsText optimized"> Allows for the check to take place at the most opportune time of the incapacity period to maximize the chances of an early return to work </div> */}
+                      <div id="immediate" className="toggleTitles immediateCheck" title="Allows for the check to happen in the shortest delays possible"> Immediate Check </div>
+                      {/* <div className="toggleDetailsText immediate"> Allows for the check to happen in the shortest delays possible </div> */}
+
                     <div className="toggle-1">
                       <label className="switch">
-                        <input name="optimizedCheck" type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.optimizedCheck} value={this.state.optimizedCheck} />
+                        <input onClick={this.toggleCssOptimized} name="optimizedCheck" type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.optimizedCheck} value={this.state.optimizedCheck} />
                         <div className="slider"></div>
                       </label>
                     </div>
-                    <div className="toggleTitles immediateCheck"> Immediate Check </div>
-                    <div className="toggleDetailsText immediate"> Allows for the check to happen in the shortest delays possible </div>
 
-                    <div className="toggleTitles doctorsOffice"> At the doctor's cabinet </div>
-                    <div className="toggleDetailsText doctor"> We recommend this choice as it is more respectful of the employee’s intimacy and is thus in accordacne with the positive approach that your employer has chosen to partake in </div>
+                    <div id="doctorsOffice" className="toggleTitles doctorsOffice toggleActive" title="We recommend this choice as it is more respectful of the employee’s intimacy and is thus in accordance with the positive approach that your employer has chosen to partake in"> At the doctor's cabinet </div>
+                    {/* <div className="toggleDetailsText doctor"> We recommend this choice as it is more respectful of the employee’s intimacy and is thus in accordacne with the positive approach that your employer has chosen to partake in </div> */}
+                    <div id="atHome" className="toggleTitles atHome" title="Checks at home are recommended only in cases where the employee is required to stay home"> At the employee's home </div>
+                    {/* <div className="toggleDetailsText home"> Checks at home are recommended only in cases where the employee is required to stay home </div> */}
+                    
                     <div className="toggle-2">
                       <label className="switch">
-                        <input name="atHome" type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.atHome} value={this.state.atHome} />
+                        <input onClick={this.toggleCssHome} name="atHome" type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.atHome} value={this.state.atHome} />
                         <div className="slider"></div>
                       </label>
                     </div>
-                    <div className="toggleTitles atHome"> At the employee's home </div>
-                    <div className="toggleDetailsText home"> Checks at home are recommended only in cases where the employee is required to stay home </div>
-
+                    
                   </div>
                 </fieldset>
               }
@@ -198,19 +224,19 @@ class Form extends React.Component {
               }
             </form>
 
+            {/* BUTTONS */}
+
             {currentStep === 1 &&
               <div className="button-placement">
                 <button className="next action-button" onClick={this.nextStep}> Next </button>
               </div>
             }
-
             {currentStep >= 2 && currentStep <= 4 &&
               <div className="button-placement">
                 <button className="prev action-button" onClick={this.prevStep}> Previous </button>
                 <button className="next action-button" onClick={this.nextStep}> Next </button>
               </div>
             }
-
             {currentStep === 5 &&
               <div className="button-placement">
                 <button className="prev action-button" onClick={this.prevStep}> Previous </button>
